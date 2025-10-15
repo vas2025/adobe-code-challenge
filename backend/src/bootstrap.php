3c2bbd169dfc4cf02411ca9494cb87eee1b9fc13
Vasily Igishev <vasily@igishev.com>
2025-10-15 01:11:34 -0700

    Added dymanic CORS

--- backend/src/bootstrap.php
+++ backend/src/bootstrap.php
@@ -22,7 +22,7 @@
 		$app->add( function ( $request , $handler ) {
 			$response = $handler->handle( $request );
 			return $response
-				->withHeader( 'Access-Control-Allow-Origin'  , 'http://localhost' )
+				->withHeader( 'Access-Control-Allow-Origin'  , $request->getHeaderLine('Origin') ?: '*' )
 				->withHeader( 'Access-Control-Allow-Headers' , 'X-Requested-With, Content-Type, Accept, Origin, Authorization' )
 				->withHeader( 'Access-Control-Allow-Methods' , 'GET, POST, PUT, DELETE, OPTIONS' )
 				->withHeader( 'Access-Control-Allow-Credentials' , 'true' );
